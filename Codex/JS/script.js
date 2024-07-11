c66d5abfe30e2959cc8bfbc8cf505fde5c3b1311
clemsclement <clemsclement@hotmail.fr>
2024-07-11 12:07:49 +0200

    terminer

--- Codex/JS/script.js
+++ Codex/JS/script.js
@@ -19,4 +19,14 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-scrollbar',
     },
   });
-  
+
+  //Début map
+
+  const map = L.map('map').setView([43.933333,2.15], 13);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    // attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+const marker = L.marker([43.933333,2.15]).addTo(map);
